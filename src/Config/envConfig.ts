8e912728b7ba08b072fa04ec122b29f785c411ce
saugat1070 <saugatgiri1070@gmail.com>
2025-05-16 13:00:32 +0545

    setting env configuration

---
+++ src/Config/envConfig.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+config()
+
+export const envConfig = {
+    port_number : process.env.PORT_NUMBER,
+    db_url : process.env.DB_URL as string,
+    email_user : process.env.EMAIL_USER as string,
+    email_pass : process.env.EMAIL_PASSWORD as string,
+    jwt_secret_key : process.env.JWT_SECRET_TOKEN as string
+}
