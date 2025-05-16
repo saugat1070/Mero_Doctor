7a34c723f05a335ed26c37adcbb2100b9149aab9
saugat1070 <saugatgiri1070@gmail.com>
2025-05-16 12:59:07 +0545

    model for user

---
+++ src/dataBase/Model/userModel.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+enum Gender{
+    male = "male",
+    female = "female",
+    other = "other"
+}
+
+const userSchema = new Schema({
+    email:{
+        type:String,
+        null:false,
+        unique:true
+    },
+    password:{
+        type: String,
+        unique: true,
+        null: false
+    },
+    fullName:{
+        type:String,
+        null:false
+    },
+    phoneNumber:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    gender:{
+        type:String,
+        enum:['male','female','other'],
+        default: Gender.male
+    },
+    dob:{
+        type:String
+    },
+    otp:{
+        type:String
+    },
+    role:{
+        type:String,
+        enum: ['patient','doctor','admin'],
+        default:'patient',
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.model('User',userSchema)
+
+export default User
+
