3a333e6173276903682d7d510369164793c613a8
saugat1070 <saugatgiri1070@gmail.com>
2025-05-16 12:58:46 +0545

    database connection

---
+++ src/dataBase/db_connection.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import { envConfig } from '../Config/envConfig';
+const connect_db = async ()=>{
+    await mongoose.connect(envConfig.db_url).then(()=>{
+        console.log("DataBase connected Successfully!");
+    }).catch((err)=>{
+        console.log("Something wrong in database connection!",err)
+    })
+}
+
+export default connect_db;
