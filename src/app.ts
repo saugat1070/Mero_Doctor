4cc40abfa87d66acfb36881a1482544ac3d1c39a
saugat1070 <saugatgiri1070@gmail.com>
2025-05-17 18:44:25 +0545

    adding more router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express,{Application} from 'express'
 import connect_db from './dataBase/db_connection';
 import  userRoute from '../src/Route/userRoute'
+import doctorRoute from '../src/Route/doctorRoute'
 const app:Application = express()
 app.use(express.json())
 connect_db()
 
 //for routing
-app.use('/api/auth',userRoute); // http://localhost:3000/api/user
-
+app.use('/api/auth',userRoute); // http://localhost:3000/api/auth
+app.use('/api/doctor',doctorRoute); // http://localhost:3000/api/doctor
 
 
 export default app;
