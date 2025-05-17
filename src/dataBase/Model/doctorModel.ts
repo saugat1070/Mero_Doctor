6567b06891924ccab353c6a9f1db8140b9e5b075
saugat1070 <saugatgiri1070@gmail.com>
2025-05-17 18:41:54 +0545

    creating object for doctor information

---
+++ src/dataBase/Model/doctorModel.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import User from "./userModel";
+const Schema = mongoose.Schema
+
+const doctorSchema = new Schema({
+    nameDoctor:{
+        type:String,
+        required:true,
+        null:false
+    },
+    core_education:{
+        type:String,
+        required: true
+    },
+    topic_education:{
+        type:String,
+        required: true
+    },
+    experience:{
+        type:Number,
+        defaultValue:0,
+    },
+    About:{
+        type:String
+    }
+    ,
+    appointmentFee:{
+        type:Number,
+        null:false,
+        defaultValue:0
+    },
+    isAvailable:{
+        type:Boolean,
+        defaultValue:false
+    },
+    hasGreentick:{
+        type:Boolean,
+        defaultValue:false
+    },
+    profileDoctor:{
+        type:String
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        unique:true,
+        required:false
+    }
+})
+
+const DoctorInfo = mongoose.model("DoctorInfo",doctorSchema)
+export default DoctorInfo;
