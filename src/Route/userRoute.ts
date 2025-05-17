7beef60e19e093700fb7196949eb3c84860cf68c
saugat1070 <saugatgiri1070@gmail.com>
2025-05-17 18:39:57 +0545

    update on user route

--- src/Route/userRoute.ts
+++ src/Route/userRoute.ts
@@ -1,14 +1,27 @@
-import express,{ Router } from "express";
+import express, { Router } from "express";
 import { UserController } from "../Controller/userController";
-const router:Router = express.Router()
+const router: Router = express.Router();
 import { UserMiddleware } from "../middleware/userMiddleware";
+import { storage, multer } from "../middleware/multer";
+//for image hanling
+const upload = multer({ storage: storage });
 
-//route 
-router.route('/registration').post(UserMiddleware.IsUserLoggin,UserController.CreateUser)
-router.route('/login').post(UserController.Login)
-router.route('/forget_password').post(UserController.forgetPassword)
-router.route('/verify-otp').post(UserController.verifyOtp)
-router.route('/fetch-user').get(UserMiddleware.IsUserLoggin,UserController.fetchUser)
-
+//route
+router
+  .route("/registration")
+  .post(UserController.CreateUser);
+router.route("/login").post(UserController.Login);
+router
+  .route("/updateUser")
+  .post(
+    UserMiddleware.IsUserLoggin,
+    upload.single("profileImg"),
+    UserController.updateUser
+  );
+router.route("/forget_password").post(UserController.forgetPassword);
+router.route("/verify-otp").post(UserController.verifyOtp);
+router
+  .route("/fetch-user")
+  .get(UserMiddleware.IsUserLoggin, UserController.fetchUser);
 
 export default router;
