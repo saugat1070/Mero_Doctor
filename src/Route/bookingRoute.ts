3ec6f1dd66363e4b06dcd8110d8a6547d7099124
saugat1070 <saugatgiri1070@gmail.com>
2025-05-22 23:14:28 +0545

    update on bookingRoute

--- src/Route/bookingRoute.ts
+++ src/Route/bookingRoute.ts
@@ -4,8 +4,10 @@ import BookingController from "../Controller/bookingController";
 
 const router:Router = express.Router()
 //router
-router.route('/').post(UserMiddleware.IsUserLoggin,BookingController.createBooking)
-
+router.route('/:id').post(UserMiddleware.IsUserLoggin,BookingController.createBooking)
+router.route('/').get(UserMiddleware.IsUserLoggin,BookingController.getBookingDetails)
+router.route('/payment/:id').post(UserMiddleware.IsUserLoggin,BookingController.PaymentSource)
+router.route('/payment/:id/verify-payment').post(UserMiddleware.IsUserLoggin,BookingController.verifyTranscition)
 
 
 export default router
