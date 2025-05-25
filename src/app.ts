2cf29d01bdbce636294ba2c010fe5abe48531ae7
saugat1070 <saugatgiri1070@gmail.com>
2025-05-25 11:39:03 +0545

    adding route for test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express,{Application} from 'express'
+import { Request,Response } from 'express';
 import connect_db from './dataBase/db_connection';
 import  userRoute from '../src/Route/userRoute'
 import doctorRoute from '../src/Route/doctorRoute'
+import bookingRoute from '../src/Route/bookingRoute'
 const app:Application = express()
 app.use(express.json())
 connect_db()
@@ -9,6 +11,12 @@ connect_db()
 //for routing
 app.use('/api/auth',userRoute); // http://localhost:3000/api/auth
 app.use('/api/doctor',doctorRoute); // http://localhost:3000/api/doctor
+app.use('/api/booking',bookingRoute); //http://localhost:3000/api/booking
+
+
+app.get("/api/test",(req:Request,res:Response)=>{
+    res.sendFile(__dirname+"/Static/test.html");
+})
 
 
 export default app;
