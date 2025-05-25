d6b79a0d71224c6d310be5dfc478b58b7680230f
saugat1070 <saugatgiri1070@gmail.com>
2025-05-25 11:35:14 +0545

    adding secret key for esewa

--- src/Config/envConfig.ts
+++ src/Config/envConfig.ts
@@ -6,5 +6,6 @@ export const envConfig = {
     db_url : process.env.DB_URL as string,
     email_user : process.env.EMAIL_USER as string,
     email_pass : process.env.EMAIL_PASSWORD as string,
-    jwt_secret_key : process.env.JWT_SECRET_TOKEN as string
+    jwt_secret_key : process.env.JWT_SECRET_TOKEN as string,
+    esewa_secret_key : process.env.ESEWA_SECRET_KEY as string
 }
