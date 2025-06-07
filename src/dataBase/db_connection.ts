ed9d69d26d230bc68e4a241dc3dba256d00187ed
saugat1070 <saugatgiri1070@gmail.com>
2025-06-07 09:21:04 +0545

    removing undefined from mongose db

--- src/dataBase/db_connection.ts
+++ src/dataBase/db_connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import { envConfig } from '../Config/envConfig';
 const connect_db = async ()=>{
-    await mongoose.connect(envConfig.db_url).then(()=>{
+    await mongoose.connect(envConfig?.db_url).then(()=>{
         console.log("DataBase connected Successfully!");
     }).catch((err)=>{
         console.log("Something wrong in database connection!",err)
