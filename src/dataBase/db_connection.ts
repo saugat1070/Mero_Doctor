78a0a1371d2d217ef3e06046ab109ec505e697f1
saugat1070 <saugatgiri1070@gmail.com>
2025-06-07 09:33:38 +0545

    console db_url

--- src/dataBase/db_connection.ts
+++ src/dataBase/db_connection.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import { envConfig } from '../Config/envConfig';
 const connect_db = async ()=>{
-    await mongoose.connect(envConfig?.db_url).then(()=>{
+    console.log(envConfig.db_url)
+    await mongoose.connect(envConfig.db_url).then(()=>{
         console.log("DataBase connected Successfully!");
     }).catch((err)=>{
         console.log("Something wrong in database connection!",err)
